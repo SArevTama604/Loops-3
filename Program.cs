2a8eaa8c9a7654956e3f2c81b492001a25a19323
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-26 22:11:13 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number to calculate its factorial: ");
+        int number;
+        while (!int.TryParse(Console.ReadLine(), out number) || number < 0)
+        {
+            Console.WriteLine("Please enter a non-negative integer: ");
+        }
+
+        int result = Factorial(number);
+        Console.WriteLine($"Factorial of {number} is: {result}");
+    }
+
+    static int Factorial(int n)
+    {
+        int factorial = 1;
+        for (int i = n; i >= 1; i--)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
